eb16772e593766c1e39f760cba460987799b0568
Brunasantitos <santitos@outlook.com.br>
2023-07-11 14:40:59 -0300

    configurando CSRF

--- cadastros_usuarios/app_cadastros_usuarios/models.py
+++ cadastros_usuarios/app_cadastros_usuarios/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Usuarios(models.Model):
+class Usuario(models.Model):
     id_usuario = models.AutoField(primary_key=True)
     nome = models.TextField(max_length=255)
     idade = models.IntegerField()
--- cadastros_usuarios/app_cadastros_usuarios/views.py
+++ cadastros_usuarios/app_cadastros_usuarios/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .models import Usuario
 # request é um comando embutido em django, permite acessar os dados dentro da pagina
 
 
@@ -7,4 +7,18 @@ def home(request):
     return render(request,'usuarios/home.html')
 
 def usuarios(request):
-    pass
+    #salvar os dados da tela para o banco de dados
+    novo_usuario = Usuario()
+    novo_usuario.nome = request.POST.get('nome')
+    novo_usuario.idade = request.POST.get('idade')
+    novo_usuario.save()
+    #Exibir todos os usuarios já cadastrados em uma nova pasta
+    usuarios = {
+        'usuarios': Usuario.objects.all()
+    }
+    #retornar os dados par a página de listagem de usuários
+    return render(request, 'usuarios/usuarios.html', usuarios)
+
+    
+
+    
